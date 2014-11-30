e3972f844c8c5d192836e73b6c445a6a3d9f9186
Stefano Schiavi <stefano@bvprojects.org>
2014-11-30 00:01:33 -0800

    add resful user edit update show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,26 @@
-get '/users/new' do
+get '/sign_up' do
   @user = User.new
-  erb :sign_in
+  erb :sign_up
 end
 
-post '/users' do
+post '/sign_up' do
   @user = User.new(params[:user])
   if @user.save
     session[:user_id] = @user.id
     redirect to '/'
   else
-    erb :sign_in
+    erb :sign_up
   end
+end
+
+get '/users/:id' do
+
+end
+
+get 'users/:id/edit' do
+
+end
+
+put 'users/:id' do
+
 end
